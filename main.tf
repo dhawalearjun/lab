fab4fa03515205fe667bc9003aabc0d2b924e450
unknown <dhawalearjun@gmail.com>
2021-11-09 12:39:24 +0530

    test

--- main.tf
+++ main.tf
@@ -18,3 +18,19 @@ resource  "google_compute_instance" "myvm1" {
                 }
         }
 }
+
+resource  "google_compute_instance" "myvm1" {
+        name = "mysecondvm"
+        machine_type = "f1-micro"
+        zone = "us-central1-a"
+        network_interface {
+                network = "default"
+                access_config{}
+
+        }
+        boot_disk {
+                initialize_params {
+                        image = "debian-cloud/debian-10"
+                }
+        }
+}
