bfcf5da3a7f76c75b40dd3a72f9de9981fca56a1
unknown <dhawalearjun@gmail.com>
2021-11-09 12:20:16 +0530

    test

---
+++ main.tf
@@ -0,0 +1,20 @@
+provider "google" {
+        project = "reflected-agent-331603"
+        region = "us-central1"
+}
+
+resource  "google_compute_instance" "myvm1" {
+        name = "myfirstvm"
+        machine_type = "f1-micro"
+        zone = "us-central1-a"
+        network_interface {
+                network = "default"
+                access_config{}
+
+        }
+        boot_disk {
+                initialize_params {
+                        image = "debian-cloud/debian-10"
+                }
+        }
+}
